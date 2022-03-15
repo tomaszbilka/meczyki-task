baaaac662e6545e21cce6684d9cf4d06f79941d7
Tomasz Biłka <bila4@op.pl>
2022-03-15 10:22:34 +0100

    add no found info in PostGrid

--- src/components/PostsGrid/PostGrid.js
+++ src/components/PostsGrid/PostGrid.js
@@ -4,6 +4,14 @@ import classes from './PostGrid.module.css';
 const PostGrid = (props) => {
   const { data } = props;
 
+  if (data?.length === 0) {
+    return (
+      <div className={classes.nofound}>
+        <p>No posts found!</p>
+      </div>
+    );
+  }
+
   return (
     <section className={classes.wrap}>
       {data.map((el) => (
